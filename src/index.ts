1bc01a8649c9186dea6a89b0747bf7605bda80b3
RafsanAmin <rafsanamin2020@gmail.com>
2022-03-08 17:56:40 +0600

    ok

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import express from 'express';
+import { origin } from './config/configs';
+
+const port = 80 || process.env.PORT;
+const app = express();
+
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send(`Sample node server!`);
+});
+
+app.get('/api/:id', (req, res) => {
+  const param = req.params.id;
+  const isEvenOdd = Number(param) % 2 === 0 ? 'Even' : 'Odd';
+
+  res.json({ ans: isEvenOdd, num: param });
+});
+app.listen(80, () => {
+  console.log(`Server running at port ${port}. Supporting ${origin}`);
+});
