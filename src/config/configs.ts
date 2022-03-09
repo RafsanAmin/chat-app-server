ff368be1986dc7de7f860ec7e78a52aaeccf9230
RafsanAmin <rafsanamin2020@gmail.com>
2022-03-09 17:47:31 +0600

    donw

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -2,3 +2,4 @@ import 'dotenv/config';
 
 export const origin =
   process.env.NODE_ENV === 'production' ? 'https://rafchat.herokuapp.com' : 'http://localhost:3000';
+export const domain = process.env.NODE_ENV === 'production' ? 'rafchat.herokuapp.com' : 'localhost';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
-import { origin } from './config/configs';
+import { domain, origin } from './config/configs';
 
 const port = process.env.PORT || 80;
 const app = express();
@@ -17,10 +17,15 @@ app.get('/', (req, res) => {
   res.send(`Sample node server!`);
 });
 
-app.get('/api/:id', (req, res) => {
+app.post('/api/:id', (req, res) => {
   const param = req.params.id;
   const isEvenOdd = Number(param) % 2 === 0 ? 'Even' : 'Odd';
-
+  res.cookie('hello', param, {
+    maxAge: 600 * 600 * 600,
+    httpOnly: true,
+    domain,
+    sameSite: 'strict',
+  });
   res.json({ ans: isEvenOdd, num: param });
 });
 app.listen(port, () => {
