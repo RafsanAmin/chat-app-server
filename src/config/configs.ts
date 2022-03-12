2620d97c354e207ea2c623b72eea6eb8ca4d1aa7
RafsanAmin <rafsanamin2020@gmail.com>
2022-03-12 18:02:25 +0600

    dsdsd

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -2,4 +2,5 @@ import 'dotenv/config';
 
 export const Origin =
   process.env.NODE_ENV === 'production' ? 'https://rafchat.herokuapp.com' : 'http://localhost:3000';
-export const Domain = process.env.NODE_ENV === 'production' ? '.herokuapp.com' : 'localhost';
+export const Domain =
+  process.env.NODE_ENV === 'production' ? '.rafchat.herokuapp.com' : 'localhost';
